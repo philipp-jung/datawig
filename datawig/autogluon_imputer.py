a215bad4e39c100af0665f5674b4c8c10638aa58
Philipp Jung <philipp.jung@verdane.com>
2022-04-18 12:32:01 +0200

    Delete Features Agrressively for Baran
    I completely removed the logic that implements confidence intervals from
    the code. It is not needed for the usage with Baran.
    
    This turns the module into a convenience wrapper around AutoGluon.

--- datawig/autogluon_imputer.py
+++ datawig/autogluon_imputer.py
@@ -14,23 +14,21 @@
 """
 
 AutoGluon Imputer:
-Imputes missing values in tables based on autogluon-tabular
+Imputes missing values in tables based on autogluon-tabular.
+
+Originally, the Imputer contained sophisticated outlier detection. Philipp
+stripped that completely and turned the module into a convenience wrapper
+around AutoGluon.
 
 """
 import os
 import pickle
 import inspect
-import warnings
-import platform
 from autogluon.tabular import TabularPredictor
 
-from typing import List, Dict, Any, Callable
+from typing import List, Any
 
 import pandas as pd
-from pandas.api.types import is_numeric_dtype
-from sklearn.metrics import precision_recall_curve, classification_report, mean_absolute_error
-from sklearn.model_selection import train_test_split
-
 
 class TargetColumnException(Exception):
     """Raised when a target column cannot be used as label for a supervised learning model"""
@@ -41,19 +39,13 @@ class AutoGluonImputer():
 
     """
 
-    AutoGluonImputer model
+    AutoGluonImputer
 
     :param model_name: name of the AutoGluonImputer (as tring)
     :param input_columns: list of input column names (as strings)
     :param output_column: output column name (as string)
     :param verbosity: verbosity level from 0 to 2
     :param output_path: path to which the AutoGluonImputer is saved to
-    :param force_multiclass: treat target-column as a multiclass problem
-    :param label_count_threshold: the minimum number of times a label
-           must appear in dataset in order to be considered an output class
-    Example usage:
-
-
     """
 
     def __init__(self,
@@ -61,9 +53,7 @@ class AutoGluonImputer():
                  input_columns: List[str] = None,
                  output_column: str = None,
                  verbosity: int = 0,
-                 output_path: str = '',
-                 force_multiclass: bool = False,
-                 label_count_threshold: int = 10,
+                 output_path: str = ''
                  ) -> None:
 
         self.model_name = model_name
@@ -71,19 +61,7 @@ class AutoGluonImputer():
         self.output_column = output_column
         self.verbosity = verbosity
         self.predictor = None
-        self.predictor_mean_absolute_error = None
         self.output_path = '.' if output_path == '' else output_path
-        self.force_multiclass = force_multiclass
-        self.learner_kwargs = {
-                'label_count_threshold': label_count_threshold,
-                'positive_class': None,
-                'ignored_columns': None,
-                'cache_data': True
-                }
-
-    @property
-    def imputed_column_name(self):
-        return str(self.output_column) + "_imputed"
 
     @property
     def datawig_model_path(self):
@@ -98,9 +76,7 @@ class AutoGluonImputer():
     def fit(self,
             train_df: pd.DataFrame,
             test_df: pd.DataFrame = None,
-            test_split: float = .1,
-            time_limit: int = 30,
-            numerical_confidence_quantile=.05) -> Any:
+            time_limit: int = 30) -> Any:
         """
 
         Trains AutoGluonImputer model for a single column
@@ -113,190 +89,34 @@ class AutoGluonImputer():
         :param time_limit: time limit for AutoGluon in seconds
         """
 
-        p = platform.system()
-        exclude_models = ['CAT']
-        # due to libopm issue on OSX described here
-        # https://github.com/awslabs/autogluon/issues/1296
-        if p == 'Darwin':
-            exclude_models = exclude_models + ['GBM', 'XGB']
+        if self.input_columns is None:
+            self.input_columns = [c for c in train_df.columns if c is not self.output_column]
 
-        if test_df is None:
-            train_df, test_df = train_test_split(train_df.copy(),
-                                                 test_size=test_split)
+        if train_df[self.output_column].value_counts().max() < 10:
+            raise TargetColumnException("Maximum class count below 10, "
+                                        "cannot train imputation model")
 
-        if not self.input_columns or len(self.input_columns) == 0:
-            self.input_columns = [
-                c for c in train_df.columns if c is not self.output_column]
-
-        if self.force_multiclass or not is_numeric_dtype(train_df[self.output_column]):
-            if train_df[self.output_column].value_counts().max() < 10:
-                raise TargetColumnException("Maximum class count below 10, "
-                                            "cannot train imputation model")
-
-            self.predictor = TabularPredictor(label=self.output_column,
-                                              problem_type='multiclass',
-                                              path=self.ag_model_path,
-                                              verbosity=self.verbosity,
-                                              learner_kwargs=self.learner_kwargs).\
-                fit(
-                    #train_data=train_df.dropna(subset=[self.output_column]),
-                    train_data=train_df,
-                    tuning_data=test_df,
-                    time_limit=time_limit,
-                    verbosity=self.verbosity,
-                    excluded_model_types=exclude_models,
-                    #hyperparameters=kwargs['hyperparameters'],
-                    #hyperparameter_tune_kwargs=kwargs['hyperparameter_tune_kwargs']
-                    )
-            y_test = test_df.dropna(subset=[self.output_column])
-
-            # prec-rec curves for finding the likelihood thresholds for minimal
-            # precision.
-            self.precision_thresholds = {}
-            probas = self.predictor.predict_proba(y_test)
-
-            for col_name in list(probas.columns):
-                prec, rec, thresholds = precision_recall_curve(y_test[self.output_column]==col_name,
-                                                               probas[col_name],
-                                                               pos_label=True)
-                self.precision_thresholds[col_name] = {'precisions': prec, 'thresholds': thresholds}
-
-            self.classification_metrics = classification_report(y_test[self.output_column],
-                                                                self.predictor.predict(y_test))
-
-        else:
-            if numerical_confidence_quantile == 0.:
-                quantile = 1e-5
-            else:
-                quantile = numerical_confidence_quantile
-
-            self.quantiles = [quantile, .5, 1-quantile]
-
-            self.predictor = TabularPredictor(
-                label=self.output_column,
-                path=self.ag_model_path,
-                quantile_levels=self.quantiles,
-                problem_type='quantile',
-                verbosity=self.verbosity)\
-                .fit(train_data=train_df.dropna(subset=[self.output_column]),
-                     time_limit=time_limit)
-
-            y_test = test_df[self.output_column].dropna()
-            y_pred = self.predictor.predict(
-                test_df.dropna(subset=[self.output_column]))
-            self.predictor_mean_absolute_error = mean_absolute_error(
-                y_test, y_pred[self.quantiles[1]])
+        self.predictor = TabularPredictor(label=self.output_column,
+                                      problem_type='multiclass',
+                                      path=self.ag_model_path,
+                                      verbosity=self.verbosity).\
+        fit(train_data=train_df,
+            tuning_data=test_df,
+            time_limit=time_limit,
+            verbosity=self.verbosity)
 
         return self
 
-    def predict(self,
-                data_frame: pd.DataFrame,
-                precision_threshold: float = 0.0,
-                numerical_confidence_interval: float = 1.0,
-                imputation_suffix: str = "_imputed",
-                inplace: bool = False,
-                return_probas: bool = False):
+    def predict_proba(self, data_frame: pd.DataFrame):
         """
-        Imputes most likely value if it is above a certain precision threshold determined on the
-            validation set
-
-        Returns original dataframe with imputations and respective likelihoods as estimated by
-        imputation model; in additional columns; names of imputation columns are that of the label
-        suffixed with `imputation_suffix`, names of respective likelihood columns are suffixed
-        with `score_suffix`
-
-        :param data_frame:   data frame (pandas)
-        :param precision_threshold: double between 0 and 1 indicating precision threshold categorical imputation
-        :param numerical_confidence_interval: double between 0 and 1 indicating confidence quantile for numerical imputation
-        :param imputation_suffix: suffix for imputation columns
-        :param inplace: add column with imputed values and column with confidence scores to data_frame, returns the
-            modified object (True). Create copy of data_frame with additional columns, leave input unmodified (False).
-        :return: data_frame original dataframe with imputations and likelihood in additional column
+        Run the imputer on a data_frame and return class probabilities.
         """
         if not self.predictor:
             raise ValueError("No predictor has been trained. Run .fit() first,"
                              " then continue with .predict().")
 
-        if not inplace:
-            df = data_frame.copy(deep=True)
-        else:
-            df = data_frame
-
-        if self.force_multiclass or self.predictor.info()['problem_type'] != 'quantile':
-            imputations = self.predictor.predict(df)
-            probas = self.predictor.predict_proba(df)
-            if return_probas:
-                return probas
-            for label in self.precision_thresholds.keys():
-                class_mask = (imputations == label)
-                precisions = self.precision_thresholds[label]['precisions']
-                thresholds = self.precision_thresholds[label]['thresholds']
-
-                # get index of first fitted threshold that surpasses threshold
-                precision_above = (precisions >= precision_threshold).nonzero()[0][0]
-                threshold_for_minimal_precision = thresholds[min(precision_above, len(thresholds)-1)]
-                if precision_threshold > 0:
-                    above_precision = class_mask & \
-                        (probas[label] >= threshold_for_minimal_precision)
-                else:
-                    above_precision = class_mask
-                df.loc[above_precision, str(self.output_column) + imputation_suffix] = label
-        else:
-            imputations = self.predictor.predict(df)
-            if self.quantiles[0] > 0:
-                confidence_tube = imputations[self.quantiles[2]]
-                - imputations[self.quantiles[0]]
-                error_smaller_than_confidence_tube = confidence_tube > self.predictor_mean_absolute_error
-                df.loc[error_smaller_than_confidence_tube, self.imputed_column_name] = \
-                    imputations.loc[error_smaller_than_confidence_tube,
-                                    self.quantiles[1]]
-
-        return df
-
-    @staticmethod
-    def complete(data_frame: pd.DataFrame,
-                 precision_threshold: float = 0.95,
-                 numerical_confidence_quantile=0.05,
-                 model_name: str = "complete_model",
-                 inplace: bool = False,
-                 time_limit: int = 10):
-        """
-        Given a dataframe with missing values, this function detects all imputable columns, trains an imputation model
-        on all other columns and imputes values for each missing value using AutoGluon.
-
-        :param data_frame: original dataframe
-        :param precision_threshold: precision threshold for categorical imputations (default: 0.0)
-        :param inplace: whether or not to perform imputations inplace (default: False)
-        :param output_path: path to store model and metrics
-        :return: dataframe with imputations
-        """
-
-        # TODO: should we expose temporary dir for model serialization to avoid crashes due to not-writable dirs?
-
-        missing_mask = data_frame.copy().isnull()
-
-        if inplace is False:
-            data_frame = data_frame.copy()
-
-        for output_col in list(data_frame.columns):
-
-            input_cols = list(set(data_frame.columns) - set([output_col]))
-
-            # train on all observed values
-            idx_missing = missing_mask[output_col]
-            try:
-                imputer = AutoGluonImputer(model_name=model_name,
-                                           input_columns=input_cols,
-                                           output_column=output_col,
-                                           precision_threshold=precision_threshold,
-                                           numerical_confidence_quantile=numerical_confidence_quantile,
-                                           verbosity=self.verbosity)\
-                    .fit(data_frame, time_limit=time_limit)
-                tmp = imputer.predict(data_frame)
-                data_frame.loc[idx_missing, output_col] = tmp[str(output_col) + "_imputed"]
-            except TargetColumnException:
-                warnings.warn(f'Could not train model on column {output_col}')
-        return data_frame
+        probas = self.predictor.predict_proba(data_frame)
+        return probas
 
     def save(self):
         """
